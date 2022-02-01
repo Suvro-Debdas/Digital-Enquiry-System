165195b5398535a245c04636b161a47045444518
Suvro Debdas <66643546+SuvroDebdas@users.noreply.github.com>
2022-02-01 17:36:22 +0530

    Reception Desk

---
+++ speak.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+
+def Listen():
+    
+    r = sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source,0,5)
+        
+    try:
+        Print("Recognizing...")
+        query = r.recognize_google(audio, language = "en-in")
+        print(f"You said that {query}")
+        
+    except:
+        return none
+    
+    query = str(query)
+    return query.lower()
